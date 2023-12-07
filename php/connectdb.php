6d3a78be561e397eb2a390eb77f462443471894d
Noor203 <143953616+Noor203@users.noreply.github.com>
2023-12-07 22:28:10 +0000

    homepage php added

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,13 +1,12 @@
 <?php
 
-$db_host = '';
-$db_name = '';
-$username = '';
+$db_host = 'localhost';
+$db_name = 'unitop';
+$username = 'root';
 $password = '';
 
 try {
 	$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password); 
-    echo("You are connected");
 } catch(PDOException $ex) {
 	echo("Failed to connect to database.<br>");
 	echo($ex->getMessage());
---
+++ php/featured.php
@@ -0,0 +1,27 @@
+<style><?php include('../css/home-page.css'); ?></style>
+<?php
+require_once('connectdb.php');
+$query = "SELECT product_id, product_name, price FROM products";
+$products = $db->query($query);
+
+
+if($products->rowCount()>0){
+    while($laptop = $products->fetch()){
+        echo"<section class='products'>
+        <a href='product-details.php?id=".$laptop['product_id']."'>
+        <img src='assests/Product/".$laptop['product_id'].".png' alt=''>
+        <h4>".$laptop['product_name']."</h4>
+        <p>£".$laptop['price']."</p>
+        </a>
+        <button class='button'>Quick Add</button>
+        </section>";
+    }
+
+}
+
+
+
+
+
+
+?>
