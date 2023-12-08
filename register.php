e6ef8daf14166f6ab26c00f2e77bea56fb2f1909
Ashraf2023Uni <148330388+Ashraf2023Uni@users.noreply.github.com>
2023-12-08 21:39:31 +0000

    Add files via upload

---
+++ register.php
@@ -0,0 +1,61 @@
+<?php
+
+$db_host = 'ask Noor';
+$db_name = 'ask Noor';
+$username = 'ask Noor';
+$password = 'ask Noor';
+
+
+$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password); 
+
+if (isset($_POST['submitted'])){
+   
+     require_once('connectdb.php');
+
+
+
+     $phoneNumber=isset($_POST['phoneNumber'])?$_POST['phoneNumber']:false;
+  
+     $email=isset($_POST['email'])?$_POST['email']:false;
+     $password=isset($_POST['password'])?password_hash($_POST['password'],PASSWORD_DEFAULT):false;
+
+
+
+     
+     if (!($phoneNumber)){
+       echo "Your number is not valid or already taken";
+       exit;
+
+     }
+
+
+
+     
+     if (!($email)){
+       echo "Your Email is not valid or already taken";
+       exit;
+
+     }
+     if (!($password)){
+       exit("PassWord is not valid");
+     }
+    }
+
+
+    try{ 
+	
+        $stat=$db->prepare("insert into signup values(default,?,?,?)");
+        $stat->execute(array($email, $password, $phoneNumber));
+        
+        $id=$db->lastInsertId();
+        echo "You are registered. Your ID is: $id  ";  	
+        
+     }
+     catch (PDOexception $ex){
+        echo "A database error has occurred. <br>";
+        echo "Error details: <em>". $ex->getMessage()."</em>";
+     }
+    
+    
+
+?>
