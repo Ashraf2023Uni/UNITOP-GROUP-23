e9eb2acba7689b7ad3a544145364f83629bf2ca8
sabil2 <220192905@aston.ac.uk>
2023-12-10 22:29:34 +0000

    updating database

--- unitop.sql
+++ unitop.sql
@@ -64,18 +64,53 @@ CREATE TABLE `products` (
   `discount_percent` decimal(3,1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
-CREATE TABLE `login` (
-  `Email` varchar(30) NOT NULL,
-  `password` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
+--- Mohammed Sabil Ali 220192905
+--
+-- Table structure for table `signup`
+--
 
 CREATE TABLE `signup` (
-  `Email` varchar(30) NOT NULL,
-  `university` varchar(30) NOT NULL,
-  `password` varchar(10) NOT NULL, 
-  `phoneNumber` decimal(12,0) NOT NULL
+  `id` int(11) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `university` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phoneNumber` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `signup`
+--
+
+INSERT INTO `signup` (`id`, `Email`, `university`, `password`, `phoneNumber`) VALUES
+(1, 'testing123@gmail.com', 'Arts University Plymouth', '$2y$10$Dzx64yUKzVKC6DQuikMcLesXOMS8A8wwTMgo/jgKQsWY7RHY9MLjy', '1234567890'),
+(2, 'testing123@gmail.com', 'Arts University Plymouth', '$2y$10$/Lh/f8O5cNC2UaSu7/dCNeERWIfiTiKA3IIfVXnXrnNF/Kqi1cVYO', '1234567890'),
+(3, 'Test1233@test.com', 'Birmingham City University', '$2y$10$LHQTET58mIIaFxMaZHmDve9.ljk6OjfYsyHLhCQrAmKSbvHPDLC3y', '123456789010'),
+(4, 'test1@hotmail.com', 'Aston University', '$2y$10$4kgRssV2QuJ7aOSGSuN1CecHSHjXgB3i7KkQ19yrdpDvTGnHeIaQe', '0121000000'),
+(5, 'sabil@hotmail.com', 'Aston University', '$2y$10$GDQCSWNaaS4jj4MoCETYR.JU09mcYBXVD31cLIigmDUhP2UkTpV9y', '07724584106'),
+(6, 'ashraf@gmail.com', 'University of Warwick', '$2y$10$VlOJicibWTP2sEpQ2S35NeTmc4pFTALOKroN3pxH.Z8go8fo3Rm7i', '041034201301');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `signup`
+--
+ALTER TABLE `signup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
 
 
 --
@@ -128,9 +163,6 @@ ALTER TABLE `products`
 --
 -- Constraints for dumped tables
 --
-ALTER TABLE `signup`
-  ADD PRIMARY KEY (`Email`);
-COMMIT;
 
 --
 -- Constraints for table `orders`
