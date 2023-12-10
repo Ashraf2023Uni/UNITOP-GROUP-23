d7269f51a99133eb13323a6115a6574f9ace390d
Humayra Tahira Hussain <humayra.h@hotmail.com>
2023-12-10 21:14:35 +0000

    nav bar links

--- product-details.php
+++ product-details.php
@@ -112,52 +112,52 @@
     <!--Product Details Body - JavaScript needed to be able to select any product and go to product detail page-->
 
 
-<!-------------------FOOTER---------------------->
-<footer>
-<div class="footer">
-    <div class="footer-box">
-        <img src="assests/Navbar/logo-no-slogan.png">
-        <h3>UNITOP</h3>
-        <p>Educate with UNITOP!</p>
-        <a href="login.php" class="btn">Log In</a>
-    </div>
-
-    <div class="footer-box">
-        <h3>Follow Us</h3>
-        <div class="socials">
-            <img src="assests/Footer/instagram.png">
-            <img src="assests/Footer/facebook.png">
-            <img src="assests/Footer/linkedin.png"> 
+   <!-------------------FOOTER---------------------->
+   <footer>
+    <div class="footer">
+        <div class="footer-box">
+            <img src="assests/Navbar/logo-no-slogan.png">
+            <h3>UNITOP</h3>
+            <p>Educate with UNITOP!</p>
+            <a href="login.php" class="btn">Log In</a>
+        </div>
+    
+        <div class="footer-box">
+            <h3>Follow Us</h3>
+            <div class="socials">
+                <img src="assests/Footer/instagram.png">
+                <img src="assests/Footer/facebook.png">
+                <img src="assests/Footer/linkedin.png"> 
+            </div>
+        </div>
+    
+        <div class="footer-box">
+            <h3>About Us</h3>
+            <ul>
+                <li><a href="about-us.html">Who We Are</a></li>
+                <br>
+                <li><a href="about-us.html">Our Mission</a></li>
+                <br>
+                <li><a href="about-us.html">The Team</a></li>
+            </ul>
+        </div>
+    
+        <div class="footer-box">
+            <h3>Useful Links</h3>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <br>
+                <li><a href="contact.html">Contact Us</a></li>
+                <br>
+                <li><a href="about-us.html">About Us</a></li>
+            </ul>
         </div>
     </div>
-
-    <div class="footer-box">
-        <h3>About Us</h3>
-        <ul>
-            <li><a href="about-us.html">Who We Are</a></li>
-            <br>
-            <li><a href="about-us.html">Our Mission</a></li>
-            <br>
-            <li><a href="about-us.html">The Team</a></li>
-        </ul>
+    <div class="line">
+          <p>Terms and Conditions apply* | UNITOP Limited</p>
     </div>
-
-    <div class="footer-box">
-        <h3>Useful Links</h3>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <br>
-            <li><a href="contact.html">Contact Us</a></li>
-            <br>
-            <li><a href="about-us.html">About Us</a></li>
-        </ul>
-    </div>
-</div>
-<div class="line">
-      <p>Terms and Conditions apply* | UNITOP Limited</p>
-</div>
-
-</footer> 
+    
+    </footer>
     
 </body>
 </html>
--- register.php
+++ register.php
@@ -130,52 +130,52 @@ echo "Error details: <em>" . $ex->getMessage() . "</em>";
 
        </div>
 
-      <!-------------------FOOTER---------------------->
-<footer>
-<div class="footer">
-    <div class="footer-box">
-        <img src="assests/Navbar/logo-no-slogan.png">
-        <h3>UNITOP</h3>
-        <p>Educate with UNITOP!</p>
-        <a href="login.php" class="btn">Log In</a>
-    </div>
-
-    <div class="footer-box">
-        <h3>Follow Us</h3>
-        <div class="socials">
-            <img src="assests/Footer/instagram.png">
-            <img src="assests/Footer/facebook.png">
-            <img src="assests/Footer/linkedin.png"> 
+   <!-------------------FOOTER---------------------->
+   <footer>
+    <div class="footer">
+        <div class="footer-box">
+            <img src="assests/Navbar/logo-no-slogan.png">
+            <h3>UNITOP</h3>
+            <p>Educate with UNITOP!</p>
+            <a href="login.php" class="btn">Log In</a>
+        </div>
+    
+        <div class="footer-box">
+            <h3>Follow Us</h3>
+            <div class="socials">
+                <img src="assests/Footer/instagram.png">
+                <img src="assests/Footer/facebook.png">
+                <img src="assests/Footer/linkedin.png"> 
+            </div>
+        </div>
+    
+        <div class="footer-box">
+            <h3>About Us</h3>
+            <ul>
+                <li><a href="about-us.html">Who We Are</a></li>
+                <br>
+                <li><a href="about-us.html">Our Mission</a></li>
+                <br>
+                <li><a href="about-us.html">The Team</a></li>
+            </ul>
+        </div>
+    
+        <div class="footer-box">
+            <h3>Useful Links</h3>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <br>
+                <li><a href="contact.html">Contact Us</a></li>
+                <br>
+                <li><a href="about-us.html">About Us</a></li>
+            </ul>
         </div>
     </div>
-
-    <div class="footer-box">
-        <h3>About Us</h3>
-        <ul>
-            <li><a href="about-us.html">Who We Are</a></li>
-            <br>
-            <li><a href="about-us.html">Our Mission</a></li>
-            <br>
-            <li><a href="about-us.html">The Team</a></li>
-        </ul>
+    <div class="line">
+          <p>Terms and Conditions apply* | UNITOP Limited</p>
     </div>
-
-    <div class="footer-box">
-        <h3>Useful Links</h3>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <br>
-            <li><a href="contact.html">Contact Us</a></li>
-            <br>
-            <li><a href="about-us.html">About Us</a></li>
-        </ul>
-    </div>
-</div>
-<div class="line">
-      <p>Terms and Conditions apply* | UNITOP Limited</p>
-</div>
-
-</footer>
+    
+    </footer>
 
     <script src="register.js"></script>
 
