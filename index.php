98aa04c9a6afc0e9ba0a0622f9ebeb020c319a2b
Humayra Tahira Hussain <humayra.h@hotmail.com>
2023-12-10 20:24:24 +0000

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    session_start();
-    require('connectdb.php');
+    if(session_status() == PHP_SESSION_NONE) {
+        session_start();
+        require('connectdb.php');
+    }
 ?>
 
 <!DOCTYPE html>
