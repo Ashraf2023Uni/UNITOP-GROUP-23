7e0e55ab36ca78f0e3ac63555ee17639ace61017
Noor203 <143953616+Noor203@users.noreply.github.com>
2023-12-10 06:06:46 +0000

    Order system working (still needs slight touch)

--- php/store-payments.php
+++ php/store-payments.php
@@ -17,12 +17,31 @@ VALUES ('$card_num', '$cvv', '$email',
 
 try{
     $db->query($query);
-    echo"Data successfully stored";
-    echo"<br><br><a href='../index.php'><button> Back to Homepage </button></a>"; }
+    echo"Payment data successfully stored";
+    echo"<br><br><a href='../index.php'><button> Back to Homepage </button></a><br>"; }
 catch(PDOException $ex){
     echo"Failed to store data <br>";
     echo($ex->getMessage());
-    echo"<br><br> <a href='../payments.php'><button> Re-enter payment method </button></a>";
-    echo"<br><br> <a href='../index.php'><button> Back to Homepage </button></a >";
+    echo"<br><br> <a href='../payments.php'><button> Re-enter payment method </button></a><br>";
+    echo"<br><br> <a href='../index.php'><button> Back to Homepage </button></a ><br>";
+}
+
+session_start();
+require_once('connectdb.php');
+try{
+for($i=0;$i<count($_SESSION['prod_id']);$i++){
+    $id = $_SESSION['prod_id'][$i];
+    $order = "SELECT stock FROM products WHERE product_id = $id";
+    $current_stock = $db->query($order)->fetch();
+    $new_stock = intval($current_stock['stock']) - intval($_SESSION['qty'][$i]);
+    $enter_stock = "UPDATE products SET stock = '$new_stock' WHERE product_id = $id";
+    echo $new_stock;
+    $db->query($enter_stock);
+}
+echo "Order was successful";
+}
+catch(PDOException $ex){
+    echo "Purchase failed";
+    echo($ex->getMessage());
 }
 ?>
