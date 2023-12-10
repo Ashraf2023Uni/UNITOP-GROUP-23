f35c4bdc4e06a7c896209082f8fe323f1099ba1b
Ashraf2023Uni <148330388+Ashraf2023Uni@users.noreply.github.com>
2023-12-10 18:12:08 +0000

    Update register.php

--- register.php
+++ register.php
@@ -1,66 +1,33 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $db_host = 'localhost';
-$db_name = 'unitop';
-$username = 'root';
-$password = '';
+$db_name = 'u_220032003_db';
+$username = 'u-220032003';
+$password = 'szHRphD4DvTQYXz';
 
+$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
 
-$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password); 
+If(isset($_POST['submitted'])) {
 
-if (isset($_POST['submitted'])){
-   
-     require_once('connectdb.php');
+$phoneNumber = isset($_POST['phoneNumber']) ? $_POST['phoneNumber'] : false;
+$university = isset($_POST['university']) ? $_POST['university'] : false;
+$email = isset($_POST['Email']) ? $_POST['Email'] : false;
+$password = isset($_POST['password']) ? password_hash($_POST['password'],PASSWORD_DEFAULT) : false; 
+If(!$phoneNumber || !$university || !$email || !$password ){
+exit("Invalid data");
+}
 
+try {
+$stat = $db->prepare("INSERT INTO signup(Email, university, password, phoneNumber) VALUES (?, ?, ?, ?)");
+$stat->execute(array($email, $university, $password, $phoneNumber));
 
-
-     $phoneNumber=isset($_POST['phoneNumber'])?$_POST['phoneNumber']:false;
-     $university=isset($_POST['university'])?$_POST['university']:false;
-  
-     $email=isset($_POST['Email'])?$_POST['Email']:false;
-     $password=isset($_POST['password'])?password_hash($_POST['password'],PASSWORD_DEFAULT):false;
-
-
-
-     
-     if (!($phoneNumber)){
-       echo "Your number is not valid or already taken";
-       exit;
-
-     }
-
-      if (!($university)){
-       echo "Your uni is not valid";
-       exit;
-
-     }
-
-     
-     if (!($Email)){
-       echo "Your Email is not valid or already taken";
-       exit;
-
-     }
-     if (!($password)){
-       exit("PassWord is not valid");
-     }
-    }
-
-
-    try{ 
-	
-        $stat=$db->prepare("insert into signup values(default,?,?,?,?)");
-        $stat->execute(array($Email, $university, $password, $phoneNumber));
-        
-        $id=$db->lastInsertId();
-        echo "You are registered. Your ID is: $id  ";  	
-        
-     }
-     catch (PDOexception $ex){
-        echo "A database error has occurred. <br>";
-        echo "Error details: <em>". $ex->getMessage()."</em>";
-     }
-    
-    
-
+	header("Location: register.html");
+	exit;
+} catch (PDOException $ex) {
+echo "A database error has occurred.<br>";
+echo "Error details: <em>" . $ex->getMessage() . "</em>";
+	}
+}
 ?>
