fd6bd947c88e3739209f995b1ffdefb66df5cb0e
Ashraf2023Uni <148330388+Ashraf2023Uni@users.noreply.github.com>
2024-02-18 22:02:09 +0000

    Update admin_index.php

--- admin_index.php
+++ admin_index.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require('connectdb.php');
+    require('php/connectdb.php');
 
     if(isset($_SESSION['admin_email'])) {
         $admin_email = $_SESSION['$admin_email'];
