a43c3516617d851e063578f1c3d9dad09bf6f29c
Ashraf2023Uni <148330388+Ashraf2023Uni@users.noreply.github.com>
2024-02-18 05:50:25 +0000

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -2,5 +2,29 @@
 include_once("connectdb.php");
 
 if(isset($_POST['submit'])) {
-    $db
-}
+    $db_host = 'localhost';
+    $db_name = 'unitop';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
+    } catch (PDOException $ex) {
+
+        echo "Failed to connect.";
+        exit;
+    }
+
+    $email = isset($_POST['email']) ? $_POST['email'] : false;
+    $password = isset($_POST['password']) ? $_POST['password'] : false;
+
+    if(!$email || !$password) {
+        echo "give both.";
+        exit;
+    }
+
+    $stmt = $db->prepare("SELECT * FROM admin_users WHERE email = ?");
+    $stmt->execute(array($email));
+    $adminUser = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($adminUser)
