f08ae097191c835e9c55025a8433cd12e5c63702
Ashraf2023Uni <148330388+Ashraf2023Uni@users.noreply.github.com>
2024-02-21 07:31:34 +0000

    Update admin_register.php

--- admin_register.php
+++ admin_register.php
@@ -43,6 +43,7 @@ if(isset($_POST['submit'])) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/login.css"> <!--Mohammed Alis style sheet -->
         <title> Admin Register </title>
 </head>
 <body>
