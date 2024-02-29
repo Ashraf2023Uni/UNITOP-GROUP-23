742611be5124326ed25ac558698adecc4ac4234e
Humayra Tahira Hussain <humayra.h@hotmail.com>
2024-02-29 15:42:23 +0000

    Updated PHP to display only 5 Products

--- php/featured.php
+++ php/featured.php
@@ -4,20 +4,29 @@ require_once('connectdb.php');
 $query = "SELECT product_id, product_name, price FROM products";
 $products = $db->query($query);
 
+$count = 0;
 
 if($products->rowCount()>0){
     while($laptop = $products->fetch()){
-        echo"
-        <section class='products'>
-            <a href='product-details.php?id=".$laptop['product_id']."'>
-            
+
+        //Only show first 5 products
+        if($count < 5){
+            echo"<section class='products'>
+                <a href='product-details.php?id=".$laptop['product_id']."'>
+                
                 <img src='assests/Product/".$laptop['product_id'].".png' alt='' id='Featured-Thumbnail'>
         
                 <h4>".$laptop['product_name']."</h4>
                 <p>£".$laptop['price']."</p>
                 <button class='button'>More Details</button>
-            </a>
-        </section>";
+                </a>
+                </section>";
+            $count++;
+        } else {
+            //Break once 5 products displayed
+            break;
+        }
+    
     }
 
 }
