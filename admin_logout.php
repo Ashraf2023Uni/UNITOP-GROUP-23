27b93b6ea791e2d52e9fcf0d71a0d19219c1c305
Ashraf2023Uni <148330388+Ashraf2023Uni@users.noreply.github.com>
2024-03-19 14:12:16 +0000

    Update admin_logout.php

--- admin_logout.php
+++ admin_logout.php
@@ -3,7 +3,7 @@
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/home-page.css">
+        <link rel="stylesheet" href="css/home-page.css"> <!-- Humayras style sheet -->
         <title> Admin Logout </title>
         <style>
             body {
