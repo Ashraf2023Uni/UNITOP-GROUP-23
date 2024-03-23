8439accc5e226a5d6fc25c843a2208b50bffdaa9
Humayra Tahira Hussain <humayra.h@hotmail.com>
2024-03-23 21:58:31 +0000

    All pages styling completed
    Admin index page Icons + buttons

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -29,87 +29,58 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width", initial-scale="1.0">
-    <title>Admin Dashboard</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: black;
-            color: azure;
-        }  
-        
-        h1, h2 {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        a {
-            display: block;
-            text-align: center;
-            margin-bottom: 20px;
-            text-decoration: none;
-            color:blue;
-        }
-
-        table {
-            width: 80%;
-            margin: 0 auto;
-            border-collapse: collapse;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            background-color: palevioletred;
-        }
-
-        th, td {
-            padding: 10px;
-            border-bottom: 1px solid green;
-            text-align: left;
-        }
-        th {
-            background-color: orange;
-            color: darkblue;
-        }
-        td input[type="number"] {
-            width: 80px;
-            padding: 5px;
-        }
-
-        td button {
-            padding: 5px 10px;
-            background-color: darkmagenta;
-            color:aqua;
-            border: none;
-            cursor: pointer;
-        }
-
-        td button:hover {
-            background-color: darkgoldenrod;
-        }
-
-        .notification {
-            position: fixed;
-            top: 20px;
-            left: 50%;
-            transform: translateX(-50%);
-            padding: 10px 20px;
-            background-color: yellow;
-            color: white;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-
-        }
-    </style>
+    <title>UNITOP/ Admin Dashboard</title>
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/home-page.css"/>
+    <link rel="shortcut icon" type="icon" href="assests/Banners/logo.png"/>
 </head>
-<body>
-    <h1>Admin Dashboard</h1>
-    <a href="admin_logout.php">Logout</a>
 
-    <h2>Product Stock Management</h2>
+<body>
+    <!--Header - brand logo and navigation bar-->
+    <header>
+        <!--LOGO-->
+        <div class="navbar">
+            <img src="assests/Navbar/UT-new-logo.png" width="100px" alt="UNITOP logo">
+            
+            <!--Search bar - products to be searched through by name-->
+            <?php include('php/search.php'); ?>
+            
+            <!--NAVIGATION BAR-->
+            <div class="links">
+                <nav>
+                    <div class="img-links">
+                        <a href="index.php"><img src="assests/Navbar/home_4991416.png" class="home-icon"></a>
+                        <a href="accounts.php"><img src="assests/Navbar/avatar_9892372.png" class="account-icon"></a>
+                        <a href="basket.php"><img src="assests/Navbar/checkout_4765148.png" class="basket-icon"></a>
+                        <a href="admin_pin.php"><img src="assests/Navbar/staffpic.png" class="staff-icon"></a>
+                    </div>
+                    <div class="page-links">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="accounts.php">Account</a></li>
+                            <li><a href="basket.php">Basket</a></li>
+                            <li><a href="admin_pin.php">Staff login</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <div class="admin">
+    <h1>Product Stock Management</h1>
+    </div>
+
     <table>
         <thead>
             <tr>
-                <th>Product ID</th>
-                <th>Product Name</th>
+                <th>ID</th>
+                <th>Name</th>
                 <th>Description</th>
                 <th>Current Stock</th>
                 <th>Actions</th>
@@ -134,6 +105,57 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
         </tbody>
     </table>
 
+    <div class="container2">
+        <form action="admin_logout.php" method="post" class="logout-form">
+            <button type="submit" class="logout-button">Logout</button>
+        </form>
+    </div>
+
+
+     <!--FOOTER-->
+     <footer>
+        <div class="footer">
+            <div class="footer-box">
+                <img src="assests/Navbar/logo-no-slogan.png">
+                <h3>UNITOP</h3>
+                <p>Educate with UNITOP!</p>
+                <?php if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true): ?>
+                <a href="login.php" class="button">Log In</a>
+                <?php endif; ?>
+            </div>
+            <div class="footer-box">
+                <h3>Follow Us</h3>
+                <div class="socials">
+                    <img src="assests/Footer/instagram.png">
+                    <img src="assests/Footer/facebook.png">
+                    <img src="assests/Footer/linkedin.png">
+                </div>
+            </div>
+            <div class="footer-box">
+                <h3>About Us</h3>
+                <ul>
+                    <li><a href="about-us.html">Who We Are</a></li>
+                    <br>
+                    <li><a href="about-us.html">Our Mission</a></li>
+                    <br>
+                    <li><a href="about-us.html">The Team</a></li>
+                </ul>
+            </div>
+            <div class="footer-box">
+                <h3>Useful Links</h3>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <br>
+                    <li><a href="contact.html">Contact Us</a></li>
+                    <br>
+                    <li><a href="about-us.html">About Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="line">
+            <p>Terms and Conditions apply* | UNITOP Limited</p>
+        </div>
+    </footer>
     <!--Potentially more HTML soon -->
 
         <!--Potentially more JavaScript here-->
--- admin_index.php
+++ admin_index.php
@@ -11,174 +11,139 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UNITOP/HomePage</title>
-    <link rel="stylesheet" href="css/home-page.css">
-    <link rel="shortcut icon" type="icon" href="assests/Banners/logo.png">
-<style>
-    .logout-form {
-        text-align: center;
-        margin-top: 20px;
-    }
-    .logout-button {
-        padding: 10px 20px;
-        font-size: 16px;
-        background-color: white;
-        color: black;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;    
-    }
-    .stock-form {
-        text-align: center;
-        margin-top: 20px;
-    }
-    .stock-button {
-        padding: 10px 20px;
-        font-size: 16px;
-        background-color: white;
-        color: black;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;    
-    }
-
-    .changePw-form {
-        text-align: center;
-        margin-top: 20px;
-    }
-    .changePw-button {
-        padding: 10px 20px;
-        font-size: 16px;
-        background-color: white;
-        color: black;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;    
-    }
-    </style>
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0">
+    <title>UNITOP/Admin Dashboard</title>
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/home-page.css"/>
+    <link rel="shortcut icon" type="icon" href="assests/Banners/logo.png"/>
 </head>
 
-
 <body>
+    <!--Header - brand logo and navigation bar-->
     <header>
-        <!--NAVBAR and other header stuff goes here-->
-         <!--NAVBAR-->
-         <div class="banner">
-            <section class="navbar">
-                <img src="assests/Navbar/UT-new-logo.png" width="100px" alt="UNITOP logo">
-
-                <!--Navbar - Links to other pages-->
-                <div class="links">
-                    <nav>
-                        <div class="img-links">
-                             
-                             <a href="about-us.html"><img src="assests/Navbar/about-us.png" class="about-us-icon"></a>
-                             <a href="contact.html"><img src="assests/Navbar/notification_9383540.png" class="contact-us-icon"></a>
-                             <a href="admin_index.php"><img src="assests/Navbar/avatar_9892372.png" class="account-icon"></a>
-                             <a href="basket.php"><img src="assests/Navbar/checkout_4765148.png" class="basket-icon"></a>
-                             
-                        </div>
-                        <div class="nav-links">
-                            <ul>
-                               
-                                <li><a href="about-us.html">About Us</a></li>
-                                <li><a href="contact.html">Contact Us</a></li>
-                                <li><a href="admin_index.php">Account</a></li>
-                                <li><a href="basket.php">Basket</a></li>
-                                
-                            </ul>
-                        </div>
-                        <div class="search-bar">
-                            <input type="search" id="search" placeholder="What are you looking for?">
-                            <button type="submit"><img src="assests/Navbar/search.png" class="search-icon"></button>
-                            </div>
-                    </nav>
-
-                </div>
-            </section>
+        <!--LOGO-->
+        <div class="navbar">
+            <img src="assests/Navbar/UT-new-logo.png" width="100px" alt="UNITOP logo">
+            
+            <!--Search bar - products to be searched through by name-->
+            <?php include('php/search.php'); ?>
+            
+            <!--NAVIGATION BAR-->
+            <div class="links">
+                <nav>
+                    <div class="img-links">
+                        <a href="index.php"><img src="assests/Navbar/home_4991416.png" class="home-icon"></a>
+                        <a href="accounts.php"><img src="assests/Navbar/avatar_9892372.png" class="account-icon"></a>
+                        <a href="basket.php"><img src="assests/Navbar/checkout_4765148.png" class="basket-icon"></a>
+                        <a href="admin_pin.php"><img src="assests/Navbar/staffpic.png" class="staff-icon"></a>
+                    </div>
+                    <div class="page-links">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="accounts.php">Account</a></li>
+                            <li><a href="basket.php">Basket</a></li>
+                            <li><a href="admin_pin.php">Staff login</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </div>
-
-        
     </header>
 
+    <!--Menu with the categories based on degrees of students-->
+    <div class="menu">
+        <?php 
+        include('php/category.php');
+        $categories = getCategories($db);
+        /*print_r($categories);*/
+        foreach ($categories as $category){
+            echo "<a href='products-page.php?category={$category['category_id']}'>{$category['category']}></a>";
+        }
+        ?>
+    </div>
+
     <!------------------------------MAIN BODY--------------------------------------->
-    <div class="container">
-        <h1>Welcome, <?php echo $admin_email; ?></h1>
-        <form action="admin_logout.php" method="post" class="logout-form">
-            <button type="submit" class="logout-button">Logout</button>
-        </form>
+    <div class="container1">
+        <h3>Welcome, <?php echo $admin_email; ?></h3>
     </div>
 
-    <div class="container2">
+    <div class="admin-container">
+        <div class="container">
+            <a href="update_stock.php"><img src="assests/Admin/stock.png" id="img1" width="210px"></a>
+            <form action="update_stock.php" method="post" class="stock-form">
+                <button type="submit" class="stock-button">Adjust Stock</button>
+            </form>
+        </div>
         
-        <form action="update_stock.php" method="post" class="stock-form">
-            <button type="submit" class="stock-button">Adjust Stock</button>
-        </form>
-    </div>
+        <div class="container">
+            <a href="admin_pw_change.php"><img src="assests/Admin/password.png" id="img1" width="160px"></a>
+            <form action="admin_pw_change.php" method="post" class="changePw-form">
+                <button type="submit" class="changePw-button">Change your password</button>
+            </form>
+        </div>
 
-    <div class="container3">
-        
-        <form action="admin_pw_change.php" method="post" class="changePw-form">
-            <button type="submit" class="changePw-button">Change your password</button>
-        </form>
-    </div>
+        <div class="container">
+            <a href="admin_change_customers.php"><img src="assests/Admin/customer.png" id="img1" width="200px"></a>
+            <form action="admin_change_customers.php" method="post" class="changePw-form">
+                <button type="submit" class="changePw-button">Customer Management</button>
+            </form>
+        </div>
 
-    <div class="container3">
-        
-        <form action="admin_change_customers.php" method="post" class="changePw-form">
-            <button type="submit" class="changePw-button">Customer Management</button>
-        </form>
-    </div>
-
-    <footer> 
-    <div class="footer">
-    <div class="footer-box">
-        <img src="assests/Navbar/logo-no-slogan.png">
-        <h3>UNITOP</h3>
-        <p>Educate with UNITOP!</p>
-       
-    </div>
-
-    <div class="footer-box">
-        <h3>Follow Us</h3>
-        <div class="socials">
-            <img src="assests/Footer/instagram.png">
-            <img src="assests/Footer/facebook.png">
-            <img src="assests/Footer/linkedin.png"> 
+        <div class="container2">
+            <form action="admin_logout.php" method="post" class="logout-form">
+                <button type="submit" class="logout-button">Logout</button>
+            </form>
         </div>
     </div>
 
-    <div class="footer-box">
-        <h3>About Us</h3>
-        <ul>
-            <li><a href="">Who We Are</a></li>
-            <br>
-            <li><a href="">Our Mission</a></li>
-            <br>
-            <li><a href="">The Team</a></li>
-        </ul>
-    </div>
-
-    <div class="footer-box">
-        <h3>Useful Links</h3>
-        <ul>
-            <li><a href="">Home</a></li>
-            <br>
-            <li><a href="">Contact Us</a></li>
-            <br>
-            <li><a href="">About Us</a></li>
-        </ul>
-    </div>
-</div>
-<div class="line">
-      <p>Terms and Conditions apply* | UNITOP Limited</p>
-</div>
-
-
+     <!--FOOTER-->
+     <footer>
+        <div class="footer">
+            <div class="footer-box">
+                <img src="assests/Navbar/logo-no-slogan.png">
+                <h3>UNITOP</h3>
+                <p>Educate with UNITOP!</p>
+                <?php if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true): ?>
+                <a href="login.php" class="button">Log In</a>
+                <?php endif; ?>
+            </div>
+            <div class="footer-box">
+                <h3>Follow Us</h3>
+                <div class="socials">
+                    <img src="assests/Footer/instagram.png">
+                    <img src="assests/Footer/facebook.png">
+                    <img src="assests/Footer/linkedin.png">
+                </div>
+            </div>
+            <div class="footer-box">
+                <h3>About Us</h3>
+                <ul>
+                    <li><a href="about-us.html">Who We Are</a></li>
+                    <br>
+                    <li><a href="about-us.html">Our Mission</a></li>
+                    <br>
+                    <li><a href="about-us.html">The Team</a></li>
+                </ul>
+            </div>
+            <div class="footer-box">
+                <h3>Useful Links</h3>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <br>
+                    <li><a href="contact.html">Contact Us</a></li>
+                    <br>
+                    <li><a href="about-us.html">About Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="line">
+            <p>Terms and Conditions apply* | UNITOP Limited</p>
+        </div>
     </footer>
 </body>
 </html>
