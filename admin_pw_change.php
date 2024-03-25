a22242b733d487e87c664f20978324dbe6270055
Humayra Tahira Hussain <humayra.h@hotmail.com>
2024-03-25 00:49:07 +0000

    Password change updated
    Styling updated to keep functionality

--- admin_pw_change.php
+++ admin_pw_change.php
@@ -186,16 +186,64 @@ hashed pw will then be stored in the database.
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang ="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Admin PW C </title>
+    <meta name="viewport" content="width=device-width", initial-scale="1.0">
+    <title>UNITOP/ Admin Password Change</title>
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/home-page.css"/>
+    <link rel="shortcut icon" type="icon" href="assests/Banners/logo.png"/>
 </head>
-<body>
-    <h1> Admin Pw change </h1>
-    <a href="admin_logout.php">Logout</a>
 
+<body>
+    <!--Header - brand logo and navigation bar-->
+    <header>
+        <!--LOGO-->
+        <div class="navbar">
+            <img src="assests/Navbar/UT-new-logo.png" width="100px" alt="UNITOP logo">
+            
+            <!--Search bar - products to be searched through by name-->
+            <?php include('php/search.php'); ?>
+            
+            <!--NAVIGATION BAR-->
+            <div class="links">
+                <nav>
+                    <div class="img-links">
+                        <a href="index.php"><img src="assests/Navbar/home_4991416.png" class="home-icon"></a>
+                        <a href="accounts.php"><img src="assests/Navbar/avatar_9892372.png" class="account-icon"></a>
+                        <a href="basket.php"><img src="assests/Navbar/checkout_4765148.png" class="basket-icon"></a>
+                        <a href="admin_pin.php"><img src="assests/Navbar/staffpic.png" class="staff-icon"></a>
+                    </div>
+                    <div class="page-links">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="accounts.php">Account</a></li>
+                            <li><a href="basket.php">Basket</a></li>
+                            <li><a href="admin_pin.php">Staff login</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <!--Back button - to go back to dashboard-->
+    <div class="admin-menu">
+     <a href="admin_index.php">Admin/Dashboard</a>
+    </div>
+
+    <div class="admin">
+    <h1> Admin Password Change </h1>
+    </div>
+    <!--<a href="admin_logout.php">Logout</a>-->
+
+    <div class="container">
     <form action="" method="post">
         <label for="current_password">Current Password:</label><br>
         <input type="password" id="current_password" name="current_password" required><br><br>
@@ -206,8 +254,9 @@ hashed pw will then be stored in the database.
         <label for="confirm_new_password">Confirm New Password:</label><br>
         <input type="password" id="confirm_new_password" name="confirm_new_password" required><br><br>
 
-        <button type ="submit">Change Password</button>
+        <button type ="submit" class="pW-button">Change Password</button>
     </form>
+    </div>
     
     <?php if (!empty($error_message)) : ?>
         <p style="color: red;"><?php echo $error_message; ?></p>
@@ -216,6 +265,51 @@ hashed pw will then be stored in the database.
         <?php if (!empty($success_message)) : ?>
             <p style="color: green;"><?php echo $success_message; ?></p>
             <?php endif; ?>
-    </body>
+            
+     <!--FOOTER-->
+     <footer>
+        <div class="footer">
+            <div class="footer-box">
+                <img src="assests/Navbar/logo-no-slogan.png">
+                <h3>UNITOP</h3>
+                <p>Educate with UNITOP!</p>
+                <?php if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true): ?>
+                <a href="login.php" class="button">Log In</a>
+                <?php endif; ?>
+            </div>
+            <div class="footer-box">
+                <h3>Follow Us</h3>
+                <div class="socials">
+                    <img src="assests/Footer/instagram.png">
+                    <img src="assests/Footer/facebook.png">
+                    <img src="assests/Footer/linkedin.png">
+                </div>
+            </div>
+            <div class="footer-box">
+                <h3>About Us</h3>
+                <ul>
+                    <li><a href="about-us.html">Who We Are</a></li>
+                    <br>
+                    <li><a href="about-us.html">Our Mission</a></li>
+                    <br>
+                    <li><a href="about-us.html">The Team</a></li>
+                </ul>
+            </div>
+            <div class="footer-box">
+                <h3>Useful Links</h3>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <br>
+                    <li><a href="contact.html">Contact Us</a></li>
+                    <br>
+                    <li><a href="about-us.html">About Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="line">
+            <p>Terms and Conditions apply* | UNITOP Limited</p>
+        </div>
+    </footer>
+</body>
 </html>
 
